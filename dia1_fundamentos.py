95f78a1185f37e6ec188afe86652614094b04fd3
Heitor <bheheitor@gmail.com>
2026-02-23 16:06:08 -0300

    Dia 1 - Fundamentos Python

---
+++ dia1_fundamentos.py
@@ -0,0 +1,37 @@
+
+"""
+Exercício 1
+
+nome = input("Digite o seu nome:")
+idade = int(input("Digite sua idade"))
+
+print(f"Olá {nome}, você tem {idade} anos!")
+
+"""
+"""
+Exercício 2
+
+num1 = int(input("Digite um número inteiro: "))
+num2 = int(input("Digite mais um número inteiro: "))
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print(f'''
+Soma: {soma}
+Subtração: {subtracao}
+Multiplicação: {multiplicacao}
+Divisão: {divisao}
+      ''')
+"""
+"""
+Exercício 3
+peso = int(input("Digite seu peso: "))
+altura = float(input("Digite sua altura: "))
+
+imc = peso / (altura*2)
+
+print(f"Seu IMC é {imc:.2f}")
+"""

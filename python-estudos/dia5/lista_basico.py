dac49b13976309217a47cb999d44f0e1b09ff4ca
HeitorAnzolin <bheheitor@gmail.com>
2026-03-05 16:17:37 -0300

    Update lista_basico.py

--- python-estudos/dia5/lista_basico.py
+++ python-estudos/dia5/lista_basico.py
@@ -6,4 +6,3 @@ print(lista_frutas[-1])#imprime último item na lista
 lista_vazia = [] #criando uma lista vazia
 lista_vazia.insert(0, 1) # Insere 1 como primeiro intem na lista
 lista_vazia.append(2) # Insere 2 no final da lista
-print(lista_vazia)
